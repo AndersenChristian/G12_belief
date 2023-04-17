61ddcf001f60520900a922795496d3e01942d73e
NicolaiUdbye <bj.polering@gmail.com>
2023-04-17 17:17:41 +0200

    parenthesis

--- src/InputValidation/Validation.java
+++ src/InputValidation/Validation.java
@@ -4,10 +4,10 @@ import java.util.regex.*;
 
 public class Validation {
 
-    String variable = "[A-Z][a-zA-Z]";
+    String variable = "[A-Z][a-zA-Z0-9]*]";
     String parenthesis = "[()]";
     String operator = "[&|]";
-    Pattern strREGEX = Pattern.compile(variable + operator);
+    Pattern strREGEX = Pattern.compile(parenthesis + variable + operator);
     String test = "((Abc o Bac) a (Abc imp (Abc o Bac)) a ((Bac a Abc) imp Abd))";
 
     boolean testString= Pattern.matches(strREGEX, test);
