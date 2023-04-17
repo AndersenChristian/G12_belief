98113b1e8399548f8c8e00c53d894513ac912a82
NicolaiUdbye <bj.polering@gmail.com>
2023-04-17 17:05:48 +0200

    ny og bedre måde at lave regex

--- src/InputValidation/Validation.java
+++ src/InputValidation/Validation.java
@@ -4,7 +4,10 @@ import java.util.regex.*;
 
 public class Validation {
 
-    Pattern strREGEX = Pattern.compile("[A-Z]");
+    String variable = "[A-Z][a-zA-Z]";
+    String parenthesis = "[()]";
+    String operator = "[&|]";
+    Pattern strREGEX = Pattern.compile(variable + operator);
     String test = "((Abc o Bac) a (Abc imp (Abc o Bac)) a ((Bac a Abc) imp Abd))";
 
     boolean testString= Pattern.matches(strREGEX, test);
