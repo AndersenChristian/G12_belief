572f040ee74eca384ac15b1fda1708e78418b269
NicolaiUdbye <bj.polering@gmail.com>
2023-04-17 17:41:45 +0200

    all operators added

--- src/InputValidation/Validation.java
+++ src/InputValidation/Validation.java
@@ -6,9 +6,12 @@ public class Validation {
 
     String variable = "[A-Z][a-zA-Z0-9]*]";
     String parenthesis = "[()]";
-    String operator = "[&|]";
-    Pattern strREGEX = Pattern.compile(parenthesis + variable + operator);
+    String operator = "[&|~]"+"[=>]"+"[<=>]";
+    Pattern expression = Pattern.compile(variable + " " + operator );
+    Pattern strREGEX = Pattern.compile(parenthesis + expression);
+
+
     String test = "((Abc o Bac) a (Abc imp (Abc o Bac)) a ((Bac a Abc) imp Abd))";
 
-    Matcher matchString= strREGEX.matcher(test);
+    Matcher matchString = strREGEX.matcher(test);
 }
