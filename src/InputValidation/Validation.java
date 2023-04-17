0ad40914bffa29c33d5aa747934afa997f21dcc8
NicolaiUdbye <bj.polering@gmail.com>
2023-04-17 16:50:24 +0200

    regex match

--- src/InputValidation/Validation.java
+++ src/InputValidation/Validation.java
@@ -7,5 +7,5 @@ public class Validation {
     Pattern strREGEX = Pattern.compile("[A-Z]");
     String test = "((Abc o Bac) a (Abc imp (Abc o Bac)) a ((Bac a Abc) imp Abd))";
 
-
+    boolean testString= Pattern.matches(strREGEX, test);
 }
