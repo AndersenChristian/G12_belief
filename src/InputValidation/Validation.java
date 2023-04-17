ea6e8c83ea27183c4ada72568f9b69c52bdf033e
NicolaiUdbye <bj.polering@gmail.com>
2023-04-17 16:48:41 +0200

    Regex

---
+++ src/InputValidation/Validation.java
@@ -0,0 +1,11 @@
+package InputValidation;
+
+import java.util.regex.*;
+
+public class Validation {
+
+    Pattern strREGEX = Pattern.compile("[A-Z]");
+    String test = "((Abc o Bac) a (Abc imp (Abc o Bac)) a ((Bac a Abc) imp Abd))";
+
+
+}
